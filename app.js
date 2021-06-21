2c25153e0621c12787de9a87d2ab44962dfb8dd9
Auredy Sanchez <auredysanchez@gmail.com>
2021-06-21 19:21:32 -0400

    add

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ const app = express();
 app.use(express.json()); 
 app.use(cors());
 
-app.use((req, res, next) => {
-  console.log(`${req.method} request made at ${req.url}`);
-  next();
-});
+
 
 app.use("/budgets", budgetsController);
 
